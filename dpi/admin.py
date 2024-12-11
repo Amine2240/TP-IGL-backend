eacaddd8501167caf7ad61999a8b0dfaad1be6d1
waelb2 <lw_bouguessa@esi.dz>
2024-12-12 00:04:45 +0100

    adding soins and medicaments to admin panel

--- dpi/admin.py
+++ dpi/admin.py
@@ -1,4 +1,7 @@
 # Register your models here.
 from django.contrib import admin
 
-from dpi.models import Hopital
+from dpi.models import Hopital, Medicament, Soin
+
+admin.site.register(Soin)
+admin.site.register(Medicament)
