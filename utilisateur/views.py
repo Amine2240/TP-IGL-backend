03b5113852e4efc33531074c1f12de07fbb122e0
waelb2 <lw_bouguessa@esi.dz>
2024-12-14 15:07:59 +0100

    updating login view

--- utilisateur/views.py
+++ utilisateur/views.py
@@ -1,3 +1,60 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from utilisateur.models import Utilisateur
 
 # Create your views here.
+
+
+class Login(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = Utilisateur.objects.filter(username=username).first()
+
+        if not username or not password:
+            return Response(
+                {"error": "Username and password are required"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        print(user.check_password(password))
+        if not user or not user.check_password(password):
+            raise AuthenticationFailed("Incorrect username or password")
+
+        # Generate JWT tokens
+        refresh = RefreshToken.for_user(user)
+        access_token = refresh.access_token
+
+        # Optionally set tokens in cookies (optional)
+        response = Response(
+            {
+                "message": "Login successful",
+            }
+        )
+
+        return Response({"message": user.username})
+
+    """    response.set_cookie(
+            key="auth_token",
+            value=str(access_token),
+            httponly=True,
+            secure=False,
+            samesite="Lax",
+        )
+        response.set_cookie(
+            key="refresh_token",
+            value=str(refresh),
+            httponly=True,
+            secure=False,
+            samesite="Lax",
+        )
+"""
