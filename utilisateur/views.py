ca31729744800523006fe1189778575ec333198b
waelb2 <lw_bouguessa@esi.dz>
2024-12-15 10:56:26 +0100

    updating login view

--- utilisateur/views.py
+++ utilisateur/views.py
@@ -26,30 +26,27 @@ class Login(APIView):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-        print(user.check_password(password))
         if not user or not user.check_password(password):
             raise AuthenticationFailed("Incorrect username or password")
 
-        # Generate JWT tokens
         refresh = RefreshToken.for_user(user)
         access_token = refresh.access_token
 
-        # Optionally set tokens in cookies (optional)
         response = Response(
             {
                 "message": "Login successful",
             }
         )
 
-        return Response({"message": user.username})
-
-    """    response.set_cookie(
+        response.set_cookie(
             key="auth_token",
             value=str(access_token),
             httponly=True,
             secure=False,
             samesite="Lax",
         )
+        return response
+        """
         response.set_cookie(
             key="refresh_token",
             value=str(refresh),
