5ef65fc81d76bae2a25be9da87237f035e1c071a
waelb2 <lw_bouguessa@esi.dz>
2024-12-24 13:57:37 +0100

    updating admin.py

--- dpi/admin.py
+++ dpi/admin.py
@@ -1,7 +1,9 @@
 # Register your models here.
 from django.contrib import admin
 
-from dpi.models import Hopital, Medicament, Soin
+from dpi.models import ContactUrgence, Dpi, Hopital, Medicament, Soin
 
 admin.site.register(Soin)
 admin.site.register(Medicament)
+admin.site.register(Dpi)
+admin.site.register(ContactUrgence)
