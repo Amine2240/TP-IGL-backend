8b591c1153b2ef166c747855be70baef7e22caf7
waelb2 <lw_bouguessa@esi.dz>
2024-12-24 20:19:07 +0100

    updating admin panel form

--- utilisateur/forms.py
+++ utilisateur/forms.py
@@ -1,7 +1,10 @@
+from decouple import config
 from django import forms
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import send_mail
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 from utilisateur.models import (
     Administratif,
@@ -45,15 +48,23 @@ class UtilisateurAdminForm(forms.ModelForm):
 
     def save(self, commit=True):
         user = super().save(commit=False)
+        clean_pwd = user.password
         if user.password and not user.password.startswith(
             ("pbkdf2_sha256$", "bcrypt$", "argon2$")
         ):
             user.password = make_password(user.password)
-
+        if not self.instance.pk:
+            subject = _("Soyez les bienvenus sur notre plateforme.")
+            message = _(
+                f"Bonjour {user.username},\n\nVotre compte a été créé avec succès.\n\n"
+                f"Username: {user.username}\n\nMot de passe : {clean_pwd}"
+            )
+            from_email = config("DEFAULT_FROM_EMAIL")
+            send_mail(subject, message, from_email, [user.email])
         user.save()
 
-        """
         # Add role-specific logic after saving the user
+        """
         if user.role == "medecin":
             Medecin.objects.get_or_create(user=user)
         elif user.role == "infermier":
@@ -64,5 +75,5 @@ class UtilisateurAdminForm(forms.ModelForm):
             Radiologue.objects.get_or_create(user=user)
         elif user.role == "administratif":
             Administratif.objects.get_or_create(user=user)
-"""
+        """
         return user
