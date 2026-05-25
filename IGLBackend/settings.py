bc3897ab3d0a6754f6711ebb9ed22823c96f2303
Amine Kadoum <ma_kadoum@esi.dz>
2026-05-25 22:11:41 +0100

    allowed hosts

--- IGLBackend/settings.py
+++ IGLBackend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-=b*#xhqag0ebsmdsi7d(dqj87yczc3(qr+)^acnxp_wv51gw5q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tp-igl-backend.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
